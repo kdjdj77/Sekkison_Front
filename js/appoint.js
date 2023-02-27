003706a55158e8d1623642f5233626dd896de23e
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-02-27 13:45:46 +0900

    update

--- js/appoint.js
+++ js/appoint.js
@@ -56,6 +56,8 @@ let posX, posY;
    }
 
 })(jQuery);
+
+
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
