d1cbaaed013fe95431b6a46045153acf5e67db8d
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-03-13 18:17:55 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -119,7 +119,7 @@
     let naverLogin = new naver.LoginWithNaverId(
         {
             clientId: "2MC6zwLDyYzFqjXZtAF3",
-            callbackUrl: "https://kdjdj77.github.io/Sekkison_Front/pages/home.html",
+            callbackUrl: "https://kdjdj77.github.io/Sekkison_Front/pages/apiregister.html",
             isPopup: false,
             callbackHandle: true
         }
