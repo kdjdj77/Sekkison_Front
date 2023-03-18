1b79691b3a8fdf187de3008db1b181ce8a73fa04
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-03-19 01:57:36 +0900

    update

--- js/path.js
+++ js/path.js
@@ -1,2 +1,7 @@
 const path = "http://Sekkison-env-1.eba-qrr3cbmm.ap-northeast-2.elasticbeanstalk.com";
 // const path = "http://localhost:5000";
+
+let currentUrl = window.location.href;
+if (currentUrl.startsWith("https:"))
+   location.href = currentUrl.replace(/^https:/, "http:");
+
