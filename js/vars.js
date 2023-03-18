19473e93c97477997dd0cd4abf3b3107583aa1ee
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-03-18 22:39:10 +0900

    update

--- js/vars.js
+++ js/vars.js
@@ -1,4 +1,5 @@
-const path = "http://Sekkison-env-1.eba-qrr3cbmm.ap-northeast-2.elasticbeanstalk.com";
+// const path = "http://Sekkison-env-1.eba-qrr3cbmm.ap-northeast-2.elasticbeanstalk.com";
+const path = "http://localhost:5000";
 
 $(function() {
    $(".common").load("../pages/common.html");
